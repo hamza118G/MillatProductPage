30c23b2ab335e9d6ec13e89ef9186b3499c41c15
Hamza <hamzaatiq118@gmail.com>
2023-06-02 15:59:48 +0500

    Before-Adding Updated Api

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,12 @@
 //Accordians toogle on navbar
 function toggleAccordion(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const header = event.target.closest('a');
-    header.classList.toggle('active');
+  const header = event.target.closest('a');
+  header.classList.toggle('active');
 
-    const content = header.nextElementSibling;
-    content.style.display = content.style.display === 'block' ? 'none' : 'block';
+  const content = header.nextElementSibling;
+  content.style.display = content.style.display === 'block' ? 'none' : 'block';
 }
 
 
@@ -15,16 +15,16 @@ function toggleAccordion(event) {
 
 var showInputsButton = document.getElementById("showInputsButton");
 var inputFields = [
-    document.getElementById("inputField1"),
-    document.getElementById("inputField2"),
-    document.getElementById("inputField3"),
-    document.getElementById("inputField4"),
+  document.getElementById("inputField1"),
+  document.getElementById("inputField2"),
+  document.getElementById("inputField3"),
+  document.getElementById("inputField4"),
 ];
 
 showInputsButton.addEventListener("click", function () {
-    inputFields.forEach(function (inputField) {
-        inputField.style.display = "block";
-    });
+  inputFields.forEach(function (inputField) {
+    inputField.style.display = "block";
+  });
 });
 
 
@@ -56,29 +56,29 @@ var clearIconBelow = document.getElementById('clearIconBelow');
 
 quantityInput.addEventListener('input', function () {
 
-    if (quantityInput.value.trim() !== '') {
-        clearIcon.style.display = 'block';
+  if (quantityInput.value.trim() !== '') {
+    clearIcon.style.display = 'block';
 
-    } else {
-        clearIcon.style.display = 'block';
+  } else {
+    clearIcon.style.display = 'block';
 
-    }
+  }
 });
 
 //Click on Small X Cross Button To Display None Whole Section
 clearIcon.addEventListener('click', function () {
-    quantityInput.value = '';
- 
-    inputFields.forEach(function (inputField) {
-        inputField.style.display = "none";
-        $("#quantityInput").prop('disabled', true);
+  quantityInput.value = '';
 
-    });
-    clearIcon.style.display = 'block';
-    $("#quantityInput").prop('disabled', false);
-    
+  inputFields.forEach(function (inputField) {
+    inputField.style.display = "none";
+    $("#quantityInput").prop('disabled', true);
 
-    quantityInput.focus();
+  });
+  clearIcon.style.display = 'block';
+  $("#quantityInput").prop('disabled', false);
+
+
+  quantityInput.focus();
 });
 
 
@@ -96,96 +96,92 @@ clearIcon.addEventListener('click', function () {
 
 
 
-  $("#showInputsButton").click(function() {
-    $("#quantityInput").attr('disabled', !$("#").attr('disabled'));
-  });
-
-  function disable(){
-    document.getElementById("quantityInput").disabled = "true";
-    }
+function disable() {
+  document.getElementById("quantityInput").disabled = "true";
+}
 
 
 
-    
-    $('#file').on('change', function() {
-      var file = this.files[0],
-          filename = file.name,
-          $label = $(this).next('.file-custom'),
-          $preview = $('#preview'),
-          img = document.createElement("img"),
-          reader = new FileReader();
-      
-      img.file = file;
-      img.classList.add("img-responsive");
-      $preview.html(img);
-      
-      reader.onload = (function(aImg) {
-        return function(e) {
-          aImg.src = e.target.result;
-        };
-      })(img);
-    
-      reader.readAsDataURL(file);
-      
-      $label
-        .attr('data-label', filename)
-          .addClass('active');
-    });
-  
+
+$('#file').on('change', function () {
+  var file = this.files[0],
+    filename = file.name,
+    $label = $(this).next('.file-custom'),
+    $preview = $('#preview'),
+    img = document.createElement("img"),
+    reader = new FileReader();
+
+  img.file = file;
+  img.classList.add("img-responsive");
+  $preview.html(img);
+
+  reader.onload = (function (aImg) {
+    return function (e) {
+      aImg.src = e.target.result;
+    };
+  })(img);
+
+  reader.readAsDataURL(file);
+
+  $label
+    .attr('data-label', filename)
+    .addClass('active');
+});
 
 
 
-    // Product Catergories Select2 
+
+// Product Catergories Select2 
 
 $.ajax({
-  url: 'https://950c-39-62-211-60.ngrok-free.app/api/EcommerceProduct/GetProductCategories',
+  url: 'https://aade-39-62-211-60.ngrok-free.app/api/EcommerceProduct/GetProductCategories',
 
   headers: {
-      'Content-Type': 'application/json',
-      "ngrok-skip-browser-warning": "69420",
+    'Content-Type': 'application/json',
+    "ngrok-skip-browser-warning": "69420",
   },
   method: 'GET',
   dataType: 'json',
 
   success: function (data) {
-      console.log(data);
-      var parentData = data.filter((value, index) => {
-          return !value.ParentId;
+    console.log(data);
+    var parentData = data.filter((value, index) => {
+      return !value.ParentId;
+    })
+
+
+    const childData = data.filter((value, index) => {
+      return value.ParentId;
+    })
+
+    parentData.map(test => {
+
+
+      let ParentNullList = document.getElementById("a")
+
+
+
+
+      let cdata = childData.map((value, index) => {
+        return test.Id == value.ParentId ? `<option> > ${value.Name}</option>` : false
+
+      })
+
+      ParentNullList.insertAdjacentHTML('beforeend',
+        `<option id="Parent-option" disabled="disabled""> ${test.Name}</option> <br> ${cdata}`
+      );
+
+
+
+
+
+      $('.js-data-example-ajax').select2({
+        placeholder: "Select a Parent",
+        allowClear: true
       })
 
 
-      const childData = data.filter((value, index) => {
-          return value.ParentId;
-      })
-     
-      parentData.map(test => {
-
-
-          let ParentNullList = document.getElementById("a")
-
-
-         
-
-          let cdata = childData.map((value, index) => {
-              return test.Id == value.ParentId ? `<option> > ${value.Name}</option>` : false
-           
-            })
-            
-          ParentNullList.insertAdjacentHTML('beforeend',
-              `<option id="Parent-option" disabled="disabled""> ${test.Name}</option> <br> ${cdata}`
-              );
-            
-           
-
-
-
-          $('.js-data-example-ajax').select2({
-              placeholder: "Select a Parent",
-              allowClear: true
-          })
-        
-        
-      })
+    })
 
 
 
@@ -210,42 +206,44 @@ var base64String;
 
 var arr = new Array();
 
-  var zdrop = new Dropzone('#dropzone', {
-    url: '#',
-    maxFiles: 2,
-    maxFilesize: 30,
-    addRemoveLinks: true,
-   
-    init: function() {
-   
-      this.on("removedfile", function(file) {
-        console.log('File is Removed');
+var zdrop = new Dropzone('#dropzone', {
+  url: '#',
+  maxFiles: 2,
+  maxFilesize: 30,
+  addRemoveLinks: true,
 
-arr = arr.filter(function(item) {
-    return item !== file.name
-})
-console.log(arr) 
-    }); 
-  }    
-  });
-    // Override the default 'upload' function to handle the file and convert it to a Base64 string
-    let dropzoneBase64String = '';
+  init: function () {
 
-    Dropzone.prototype.uploadFiles = function (files) {
-      const self = this;
-      for (let i = 0; i < files.length; i++) {
-        const file = files[i];
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          const base64String = e.target.result;
-          self.emit("success", file, { base64String: base64String });
-          arr.push(file.name);
-        
-          dropzoneBase64String = base64String; // Store the base64String in a variable accessible to the Approve function
-        };
-        reader.readAsDataURL(file);
-      }
-    }
+    this.on("removedfile", function (file) {
+      console.log('File is Removed');
+
+      arr = arr.filter(function (item) {
+        return item !== file.name
+      })
+      console.log(arr)
+    });
+  }
+});
+// Override the default 'upload' function to handle the file and convert it to a Base64 string
+let dropzoneBase64String = '';
+
+Dropzone.prototype.uploadFiles = function (files) {
+  const self = this;
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const base64String = e.target.result;
+      self.emit("success", file, {
+        base64String: base64String
+      });
+      arr.push(file.name);
+
+      dropzoneBase64String = base64String; // Store the base64String in a variable accessible to the Approve function
+    };
+    reader.readAsDataURL(file);
+  }
+}
 
 
 
@@ -255,7 +253,7 @@ var rowData = [];
 
 // Select2 Variation Section
 $.ajax({
-  url: 'https://950c-39-62-211-60.ngrok-free.app/api/EcommerceProduct/GetProductVariations',
+  url: 'https://aade-39-62-211-60.ngrok-free.app/api/EcommerceProduct/GetProductVariations',
   headers: {
     'Content-Type': 'application/json',
     "ngrok-skip-browser-warning": "69420",
@@ -316,12 +314,12 @@ function addRow() {
 
   var input1 = document.getElementById("variationCategory");
   var text = input1.options[input1.selectedIndex].text;
-  var input112 = document.getElementById("variationCategory").value
+  var variationCatergory = document.getElementById("variationCategory").value
 
   var input2 = document.getElementById("variationCategory-child").value;
   var input3 = document.getElementById("input3").value;
 
-  var fazool = document.getElementById('Product-Code').value
+  var SameCode = document.getElementById('Product-Code').value
   let ProductQuantity = document.getElementById("quantityInput").value;
 
 
@@ -341,12 +339,13 @@ function addRow() {
     Variation: text,
     Attribute: input2,
     Quantity: input3,
-    AttributeId:input112,
-    Code:fazool,
+    AttributeId: variationCatergory,
+    Code: SameCode,
     ProductQuantity: ProductQuantity
   };
   rowData.push(rowObj);
 }
+
 function removeRow(button) {
   var row = button.parentNode.parentNode;
   row.parentNode.removeChild(row);
@@ -357,21 +356,28 @@ function removeRow(button) {
 
 // Approve function
 function Approve() {
+
   let myobj = {};
+
+
   let ProductName = document.getElementById("Product-first-name").value;
   let ProductCode = document.getElementById("Product-Code").value;
   let ShortDescription = document.getElementById("Short-description").value;
   let FullDescription = document.getElementById("Full-description").value;
   let ProductQuantity = document.getElementById("quantityInput").value;
- 
 
 
-  
+
+
 
   var ProductPrice = Number(document.getElementById("Product-Price").value);
   var ProductDiscount = Number((document.getElementById("Product-Discount").value = 30)) / 100;
   let ProductDiscountPercentage = ProductPrice - ProductPrice * ProductDiscount;
   ProductDiscountPercentage.toFixed(2);
+
+
+
+
 
   let bestSellingCheckbox = document.getElementById("Product-Check");
   let isBestSelling = bestSellingCheckbox.checked;
@@ -379,12 +385,22 @@ function Approve() {
   let featuredCheckbox = document.getElementById("Featured-Check");
   let isFeatured = featuredCheckbox.checked;
 
+
+
+
+
+
   let aImg = document.querySelector('#preview img');
   if (aImg) {
     aImg.file;
   }
 
+
+
   let selectedCategory = $('.js-data-example-ajax').val();
+
+
+
 
   myobj["Product-first-name"] = ProductName;
   myobj["Product-Code"] = ProductCode;
@@ -399,6 +415,7 @@ function Approve() {
   myobj["BestSelling"] = isBestSelling;
   myobj["Featured"] = isFeatured;
   myobj["Variation"] = rowData;
+  myobj["DropZone-base64String"] = dropzoneBase64String
 
   console.log(myobj);
 }
